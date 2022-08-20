b92db2b2c37a5fe35ff99d7974745765cc339ddf
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-08-19 23:51:59 -0700

    Add partials to comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -39,7 +39,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
-    redirect_to root_path, status: :see_other
+    redirect_to root_path, status: :see_otherarticles/5
   end
 
   private
